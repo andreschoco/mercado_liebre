30b334b0a5a09d91ed756079ea4a2f443c1d4980
andreschoco <andreschoco@gmail.com>
2022-09-03 08:36:26 -0500

    Ejercicio parte 4 concluido

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(3000,() =>{
+    console.log('Servidor en puerto 3000 OK');
+});
+
+app.get('/', (req,res)=>{
+res.sendFile(path.join(__dirname, '/views/home.html'));
+});
