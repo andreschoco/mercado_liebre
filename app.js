13b6957b9bbce41d4a0d4ca7f2fd7ef5dadd1e66
andreschoco <andreschoco@gmail.com>
2022-09-26 21:56:50 +0800

    con shadow correcto

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.get('/login', (req, res) => {
 });
 
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Servidor corriendo");
 })
+
+
