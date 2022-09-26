1407b0fbdc48dbf8550002a4878c1655effcf4ca
andreschoco <andreschoco@gmail.com>
2022-09-26 19:45:10 +0800

    con los requrimientos basicos del ejercicio

--- app.js
+++ app.js
@@ -5,10 +5,23 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.listen(3000,() =>{
+app.listen(3000, () => {
     console.log('Servidor en puerto 3000 OK');
 });
 
-app.get('/', (req,res)=>{
-res.sendFile(path.join(__dirname, '/views/home.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
 });
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+})
